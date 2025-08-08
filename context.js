76dd3d4219d980d937267d61bb245916c41e7f52
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-08 16:57:43 +0800

    global and function context

--- context.js
+++ context.js
@@ -4,5 +4,10 @@ function add(num1, num2) {
   return result;
 }
 
+// can not access "result" because it is in 
+// console.log(result);
+
+// can access the variable "a" because it is declared in Global Execution Context
+console.log(a);
 const sum = add(4, 5);
 console.log(sum);
